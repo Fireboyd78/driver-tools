6ed0b29da39383788db2f2fd25918cdbf7299ab8
Mark Ludwig <mk.ludwig1@gmail.com>
2017-06-05 22:08:09 -0700

    Complete overhaul of vehicle hierarchy stuff. Could use a rewrite...
    Not too happy with the way the new code came out,
    but then again I didn't know much of the spec when I first wrote it.
    
    Rewriting the code required modifications to the VPK exporter.
    Since I'm not sure if I broke anything, I bumped the version up to 7. :P

--- DSCript/Models/Hierarchy/VehicleHierarchyData.cs
+++ DSCript/Models/Hierarchy/VehicleHierarchyData.cs
@@ -19,7 +19,20 @@ namespace DSCript.Models
 {
     public class VehicleHierarchyData : SpoolableResource<SpoolableBuffer>
     {
-        public enum SlotType : short
+        public static readonly MagicNumber VPKBulletMagic = "BULL";
+        public static readonly int VPKBulletVersion = 2;
+
+        public enum VehiclePartType
+        {
+            Type1       = 1,
+            Type2       = 2,
+            Marker      = 3,
+            Container   = 4,
+            Type8       = 8,
+            DamageWheel = 9,
+        }
+
+        public enum VehiclePartSlotType
         {
             Generic                 = 0x00,
             
@@ -131,37 +144,33 @@ namespace DSCript.Models
             CornerBumper            = 0x71
         }
 
-        public class PartEntry
+        [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 0x20)]
+        public struct VehiclePartData
         {
-            public short Type { get; set; }
-            
-            public SlotType SlotType { get; set; }
+            public short PartType;
+            public short SlotType;
 
-            public short Flags1 { get; set; }
-            public short Flags2 { get; set; }
+            // following flag names are only educated guesses
+            public int RenderFlags;
+            public int CullingFlags;
 
-            public short Unknown1 { get; set; }
-            public short Unknown2 { get; set; }
+            public short NumChildren;
 
-            public short Unknown3 { get; set; }
+            public short Unknown; // possibly a parent to rotate around?
+            public short Hinge;
 
-            public short Hinge { get; set; }
-            public byte PartId { get; set; }
-            public byte Unknown4 { get; set; }
-            
-            public PDLEntry Physics { get; set; }
-            public Vector4? Position { get; set; }
+            public byte ModelId;
 
-            public Thing2 Offset { get; set; }
-            public Thing3 Transform { get; set; }
+            public short PhysicsId;
+            public short PositionId;
 
-            public Thing4 Axis { get; set; }
+            public short OffsetId;
+            public short TransformId;
 
-            public short Unknown6 { get; set; }
-
-            public List<PartEntry> Children { get; set; }
+            public short AxisId;
+            public short Reserved; // ;)
         }
-
+        
         public class Thing2
         {
             public Vector4 Position { get; set; }
@@ -330,7 +339,7 @@ namespace DSCript.Models
 
                         ms.Write(PackWeight(Weight));
 
-                        ms.Write((ushort)0xDF83); // ;)
+                        ms.Write((ushort)MagicNumber.FB); // ;)
 
                         // set the material type
                         ms.Position = 11;
@@ -348,7 +357,7 @@ namespace DSCript.Models
                         ms.Write(Weight);
 
                         ms.Write((ushort)MaterialType);
-                        ms.Write((ushort)0xDF83); // ;)
+                        ms.Write((ushort)MagicNumber.FB); // ;)
                     }
                 }
                 
@@ -395,8 +404,9 @@ namespace DSCript.Models
         
         public ModelPackage ModelPackage { get; set; }
 
-        public List<PartEntry> Parts { get; set; }
+        public List<VehiclePartData> Parts { get; set; }
 
+        public List<Vector4> T1Entries { get; set; }
         public List<Thing2> T2Entries { get; set; }
         public List<Thing3> T3Entries { get; set; }
         public List<Thing4> T4Entries { get; set; }
@@ -406,7 +416,7 @@ namespace DSCript.Models
         public List<BulletHolder> BulletHolders { get; set; }
 
         public int UID { get; set; }
-        public int Reserved { get; set; }
+        public int Flags { get; set; }
 
         public int BulletCount
         {
@@ -430,42 +440,36 @@ namespace DSCript.Models
                 // skip header
                 f.Position = 0xC;
 
-                if (f.ReadInt32() != 6)
-                    throw new Exception("Cannot load hierarchy data - unsupported type");
+                var header = f.Read<HierarchyDataHeader>();
+                var vInfo = header.VehicleHierarchyInfo;
 
-                var nParts = f.ReadInt32();
+                if (header.HierarchyType != HierarchyDataType.Vehicle)
+                    throw new InvalidOperationException("Cannot load vehicle hierarchy data -- invalid hierarchy type!");
+                
+                UID = header.UID;
+                Flags = vInfo.Flags;
+                
+                var bulDataOffset = (header.PDLOffset + header.PDLSize);
 
-                UID = f.ReadInt32();
+                // read thing1 data
+                f.Position = (header.ExtraPartsDataOffset + vInfo.T1Offset);
 
-                var bulDataOffset = f.ReadInt32();
+                T1Entries = new List<Vector4>(vInfo.T1Count);
 
-                Reserved = f.ReadInt32();
+                for (int i = 0; i < vInfo.T1Count; i++)
+                {
+                    var v4 = f.Read<Vector4>();
 
-                var t1Count = f.ReadInt16();
-                var t2Count = f.ReadInt16();
-                var t3Count = f.ReadInt16();
-                var t4Count = f.ReadInt16();
-
-                // parts offset is hard-coded
-                var partsOffset = 0x30;
-
-                // calculate offsets so we can read data
-                var t2Offset = partsOffset + (nParts * 0x20);
-                var t3Offset = t2Offset + (t2Count * 0x20);
-                var t1Offset = t3Offset + (t3Count * 0x50);
-                var t4Offset = t1Offset + (t1Count * 0x10);
-                var pdlOffset = t4Offset + (t4Count * 0x40);
-
-                // make offset absolute
-                bulDataOffset += pdlOffset;
+                    T1Entries.Add(v4);
+                }
 
                 // read thing2 data
-                T2Entries = new List<Thing2>(t2Count);
+                f.Position = (header.ExtraPartsDataOffset + vInfo.T2Offset);
 
-                for (int i = 0; i < t2Count; i++)
+                T2Entries = new List<Thing2>(vInfo.T2Count);
+                
+                for (int i = 0; i < vInfo.T2Count; i++)
                 {
-                    f.Position = (i * 0x20) + t2Offset;
-
                     T2Entries.Add(new Thing2() {
                         Position = f.Read<Vector4>(),
                         Rotation = f.Read<Vector4>(),
@@ -473,12 +477,12 @@ namespace DSCript.Models
                 }
 
                 // read thing3 data
-                T3Entries = new List<Thing3>(t3Count);
+                f.Position = (header.ExtraPartsDataOffset + vInfo.T3Offset);
 
-                for (int i = 0; i < t3Count; i++)
+                T3Entries = new List<Thing3>(vInfo.T3Count);
+
+                for (int i = 0; i < vInfo.T3Count; i++)
                 {
-                    f.Position = (i * 0x50) + t3Offset;
-
                     T3Entries.Add(new Thing3() {
                         RotationX = f.Read<Vector4>(),
                         RotationY = f.Read<Vector4>(),
@@ -489,12 +493,12 @@ namespace DSCript.Models
                 }
 
                 // read thing4 data
-                T4Entries = new List<Thing4>(t4Count);
+                f.Position = (header.ExtraPartsDataOffset + vInfo.T4Offset);
 
-                for (int i = 0; i < t4Count; i++)
+                T4Entries = new List<Thing4>(vInfo.T4Count);
+
+                for (int i = 0; i < vInfo.T4Count; i++)
                 {
-                    f.Position = (i * 0x40) + t4Offset;
-
                     T4Entries.Add(new Thing4() {
                         Unknown1 = f.Read<Vector4>(),
                         Unknown2 = f.Read<Vector4>(),
@@ -504,8 +508,11 @@ namespace DSCript.Models
                 }
 
                 // read PDL data
-                f.Position = pdlOffset;
+                var pdlOffset = header.PDLOffset;
 
+                f.Position = header.PDLOffset;
+
+                // TODO: Write a struct to encapsulate this stuff
                 var entriesCount = f.ReadInt32();
                 var pdlDataCount = f.ReadInt32();
 
@@ -556,100 +563,23 @@ namespace DSCript.Models
                 }
 
                 // read parts
-                Parts = new List<PartEntry>(nParts);
+                f.Position = header.PartsOffset;
 
-                for (int i = 0; i < nParts; i++)
+                Parts = new List<VehiclePartData>(header.PartsCount);
+
+                for (int i = 0; i < header.PartsCount; i++)
                 {
-                    f.Position = (i * 0x20) + partsOffset;
-
-                    var entry = new {
-                        Type = f.ReadInt16(),
-                        SlotType = (SlotType)f.ReadInt16(),
-
-                        Flags1 = f.ReadInt16(),
-                        Flags2 = f.ReadInt16(),
-
-                        Unknown1 = f.ReadInt16(),
-                        Unknown2 = f.ReadInt16(),
-
-                        NumChildren = f.ReadInt16(),
-                        Unknown3 = f.ReadInt16(),
-
-                        Hinge = f.ReadInt16(),
-                        PartId = (byte)f.ReadByte(),
-                        Unknown4 = (byte)f.ReadByte(),
-
-                        PhysicsId = f.ReadInt16(),
-                        PositionId = f.ReadInt16(),
-
-                        OffsetId = f.ReadInt16(),
-                        TransformId = f.ReadInt16(),
-
-                        AxisId = f.ReadInt16(),
-                        Unknown6 = f.ReadInt16(),
-                    };
-
-                    var position = new Vector4();
-
-                    // get position data
-                    if (entry.PositionId != -1)
-                    {
-                        f.Position = (entry.PositionId * 0x10) + t1Offset;
-
-                        position = f.Read<Vector4>();
-                    }
-
-                    var part = new PartEntry() {
-                        Type = entry.Type,
-                        SlotType = entry.SlotType,
-
-                        Flags1 = entry.Flags1,
-                        Flags2 = entry.Flags2,
-
-                        Unknown1 = entry.Unknown1,
-                        Unknown2 = entry.Unknown2,
-
-                        Children = (entry.NumChildren > 1) ? new List<PartEntry>(entry.NumChildren - 1) : null,
-                        Unknown3 = entry.Unknown3,
-
-                        Hinge = entry.Hinge,
-                        PartId = entry.PartId,
-                        Unknown4 = entry.Unknown4,
-
-                        Physics = (entry.PhysicsId >= 0) ? PDLEntries[entry.PhysicsId] : null,
-                        Position = position,
-
-                        Offset = (entry.OffsetId >= 0) ? T2Entries[entry.OffsetId] : null,
-                        Transform = (entry.TransformId >= 0) ? T3Entries[entry.TransformId] : null,
-
-                        Axis = (entry.AxisId >= 0) ? T4Entries[entry.AxisId] : null,
-                        Unknown6 = entry.Unknown6
-                    };
-
+                    var part = f.Read<VehiclePartData>();
                     Parts.Add(part);
                 }
-
-                // add children
-                for (int i = 0; i < nParts; i++)
-                {
-                    var part = Parts[i];
-
-                    if (part.Children != null && part.Children.Capacity > 0)
-                    {
-                        var count = part.Children.Capacity;
-
-                        for (int ii = 0; ii < count; ii++)
-                            part.Children.Add(Parts[i + ii + 1]);
-                    }
-                }
-
+                
                 // read bullet hole data
                 f.Position = bulDataOffset;
 
                 var nBullets = f.ReadInt32();
                 var nHolders = f.ReadInt32(); // => t3Count
 
-                if (nHolders != t3Count)
+                if (nHolders != vInfo.T3Count)
                     throw new InvalidOperationException("UH-OH! Looks like the bullet hole data is corrupt!");
 
                 BulletHolders = new List<BulletHolder>(nHolders);
@@ -695,7 +625,7 @@ namespace DSCript.Models
         
         public void SaveVPK(string filename)
         {
-            var version = 6;
+            var version = 7;
 
             // allocate 32mb buffer
             using (var fs = new MemoryStream(1024 * 32767))
@@ -713,7 +643,7 @@ namespace DSCript.Models
                 // 0x18: skip bullet hole data offset
                 fs.Position += 0x4;
 
-                fs.Write(Reserved);
+                fs.Write(Flags);
                 
                 var isEnabled = new Func<bool, short>((b) => {
                     return (short)(b ? 1 : -1);
@@ -721,42 +651,33 @@ namespace DSCript.Models
                 
                 foreach (var part in Parts)
                 {
-                    var nChildren = 1;
+                    var hasPhysics      = (part.PhysicsId != -1);
+                    var hasPosition     = (part.PositionId != -1);
+                    var hasOffset       = (part.OffsetId != -1);
+                    var hasTransform    = (part.TransformId != -1);
+                    var hasAxis         = (part.AxisId != -1);
+                    
+                    fs.Write(part.PartType);
+                    fs.Write(part.SlotType);
 
-                    var hasPhysics  = (part.Physics != null);
-                    var hasPosition = (part.Position != null);
-                    var hasOffset   = (part.Offset != null);
-                    var hasUnknown5 = (part.Transform != null);
-                    var hasAxis     = (part.Axis != null);
+                    fs.Write(part.RenderFlags);
+                    fs.Write(part.CullingFlags);
 
-                    if (part.Children != null)
-                        nChildren += part.Children.Count;
+                    fs.Write(part.NumChildren);
 
-                    fs.Write(part.Type);
-                    fs.Write((short)part.SlotType);
-
-                    fs.Write(part.Flags1);
-                    fs.Write(part.Flags2);
-
-                    fs.Write(part.Unknown1);
-                    fs.Write(part.Unknown2);
-
-                    fs.Write((short)nChildren);
-                    fs.Write(part.Unknown3);
-
+                    fs.Write(part.Unknown);
                     fs.Write(part.Hinge);
 
-                    fs.WriteByte(part.PartId);
+                    fs.WriteByte(part.ModelId);
                     
                     if (version == 3)
                     {
-                        //Unknown4 is unused
                         fs.WriteByte(0x99);
 
                         fs.Write(isEnabled(hasPhysics));
                         fs.Write(isEnabled(hasPosition));
                         fs.Write(isEnabled(hasOffset));
-                        fs.Write(isEnabled(hasUnknown5));
+                        fs.Write(isEnabled(hasTransform));
                         fs.Write(isEnabled(hasAxis));
                     }
                     else if (version >= 4)
@@ -770,19 +691,19 @@ namespace DSCript.Models
                             flags |= 2;
                         if (hasOffset)
                             flags |= 4;
-                        if (hasUnknown5)
+                        if (hasTransform)
                             flags |= 8;
                         if (hasAxis)
                             flags |= 16;
 
                         fs.WriteByte(flags);
                     }
-
-                    fs.Write(part.Unknown6);
-
+                    
+                    fs.Write((part.Reserved != 0) ? part.Reserved : (short)MagicNumber.FB);
+                    
                     if (hasPhysics)
                     {
-                        var phy = part.Physics;
+                        var phy = PDLEntries[part.PhysicsId];
 
                         fs.Write(phy.Children.Count);
                         fs.WriteFloat(phy.Unknown);
@@ -809,19 +730,19 @@ namespace DSCript.Models
                     }
 
                     if (hasPosition)
-                        fs.Write(part.Position.Value);
+                        fs.Write(T1Entries[part.PositionId]);
 
                     if (hasOffset)
                     {
-                        var off = part.Offset;
+                        var off = T2Entries[part.OffsetId];
 
                         fs.Write(off.Position);
                         fs.Write(off.Rotation);
                     }
 
-                    if (hasUnknown5)
+                    if (hasTransform)
                     {
-                        var unk5 = part.Transform;
+                        var unk5 = T3Entries[part.TransformId];
 
                         fs.Write(unk5.RotationX);
                         fs.Write(unk5.RotationY);
@@ -832,7 +753,7 @@ namespace DSCript.Models
 
                     if (hasAxis)
                     {
-                        var axis = part.Axis;
+                        var axis = T4Entries[part.AxisId];
 
                         fs.Write(axis.Unknown1);
                         fs.Write(axis.Unknown2);
@@ -845,8 +766,6 @@ namespace DSCript.Models
                     fs.Align(16);
                 
                 var bulDataOffset = (int)fs.Position;
-
-                var bulVersion = 2;
                 var bulType = (version < 6) ? 0 : 1; // packed=0, unpacked=1
 
                 var packBullets = (bulType == 0);
@@ -863,8 +782,8 @@ namespace DSCript.Models
                 bulletLog.AppendLine();
 
                 // write bullet hole data!
-                fs.Write(0x4C4C5542); // 'BULL'
-                fs.Write((short)bulVersion);
+                fs.Write((int)VPKBulletMagic); // 'BULL'
+                fs.Write((short)VPKBulletVersion);
                 fs.Write((short)bulType);
                 
                 fs.Write(nHolders);
