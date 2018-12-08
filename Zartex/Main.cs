156ef155ae92b8ac10cbf59af6b0a88094cd6785
Mark Ludwig <mk.ludwig1@gmail.com>
2018-12-07 18:27:54 -0800

    Fixed a bug where different cultures would affect number parsing.

--- Zartex/Main.cs
+++ Zartex/Main.cs
@@ -5,10 +5,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Text;
 using System.Windows.Forms;
 
@@ -31,6 +33,12 @@ namespace Zartex
             TypeDescriptor.AddAttributes(typeof(Vector2), new TypeConverterAttribute(typeof(VectorTypeConverter)));
             TypeDescriptor.AddAttributes(typeof(Vector3), new TypeConverterAttribute(typeof(VectorTypeConverter)));
             TypeDescriptor.AddAttributes(typeof(Vector4), new TypeConverterAttribute(typeof(VectorTypeConverter)));
+
+            var culture = new CultureInfo("en-US", false);
+            var thread = Thread.CurrentThread;
+
+            thread.CurrentCulture = culture;
+            thread.CurrentUICulture = culture;
         }
 
         string title;
